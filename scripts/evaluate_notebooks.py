b03d3ce24e594d9948cefe143be44dfa2ebd5c17
kejnn <rossnc@gmail.com>
2023-07-12 13:25:36 -0500

    Adding Helper Functions for Code

---
+++ scripts/evaluate_notebooks.py
@@ -0,0 +1,16 @@
+import os
+from utils import is_git_repo, get_remote_branches_info, walk_and_process, parse_arguments
+
+if __name__ == '__main__':
+    args = parse_arguments()
+
+    if not os.path.isdir(args.dir_path):
+        print(f"Error: {args.dir_path} is not a valid directory.")
+        exit(1)
+
+    if not is_git_repo(args.dir_path):
+        print(f"Error: {args.dir_path} is not a Git repository.")
+        exit(1)
+
+    get_remote_branches_info(args.dir_path)
+    walk_and_process(args.dir_path, args.no_filter)
---
+++ scripts/utils.py
@@ -0,0 +1,91 @@
+import os
+import json
+import git
+import argparse
+import pyflakes.api
+
+def count_functions(cell):
+    """Count the number of functions defined in a Jupyter Notebook cell."""
+    code = cell['source']
+    return sum([1 for line in code if line.strip().startswith('def ')])
+
+def process_notebook(file_path):
+    """Process a Jupyter Notebook and count the cells, lines of code and functions."""
+    with open(file_path, 'r', encoding='utf-8') as f:
+        notebook = json.load(f)
+        cells = notebook['cells']
+        num_cells = len(cells)
+        num_lines = 0
+        num_functions = 0
+        max_lines_in_cell = 0
+        for cell in cells:
+            if cell['cell_type'] == 'code':
+                lines_in_cell = sum([1 for line in cell['source'] if line.strip()])
+                num_lines += lines_in_cell
+                max_lines_in_cell = max(max_lines_in_cell, lines_in_cell)
+                num_functions += count_functions(cell)
+        return num_cells, num_lines, num_functions, max_lines_in_cell
+
+def walk_and_process(dir_path, no_filter_flag):
+    """Walk through directory and process all Jupyter Notebooks."""
+    current_notebook_count = 0
+    stats_printed = 0
+    for root, dirs, files in os.walk(dir_path):
+        for file in files:
+            if file.endswith('.ipynb'):
+                current_notebook_count += 1
+                file_path = os.path.join(root, file)
+                num_cells, num_lines, num_functions, max_lines_in_cell = process_notebook(file_path)
+                if no_filter_flag or (num_cells > 10 or max_lines_in_cell > 15 or num_functions >0 ): 
+                    stats_printed += 1
+                    print(f'File: {file_path}')
+                    print(f'\tNumber of cells: {num_cells}')
+                    print(f'\tLines of code: {num_lines}')
+                    print(f'\tNumber of function definitions: {num_functions}')
+                    print(f'\tMax lines in a cell: {max_lines_in_cell}')
+                    print('-' * 40)
+    print(f"Files information printed: {stats_printed}")
+
+def run_pyflakes(directory):
+    python_files = [filename for filename in os.listdir(directory) if filename.endswith(".py")]
+
+    results = []
+    for filename in python_files:
+        file_path = os.path.join(directory, filename)
+        with open(file_path, "r") as file:
+            file_content = file.read()
+            messages = pyflakes.api.check(file_content, file_path)
+            results.extend(messages)
+
+    return results
+
+def parse_arguments():
+    """Parse command line arguments."""
+    parser = argparse.ArgumentParser(description='Process Jupyter Notebooks.')
+    parser.add_argument('dir_path', type=str, help='Directory path to start the search from')
+    parser.add_argument('--no_filter', action='store_true', help='Print stats on all files')
+    return parser.parse_args()
+
+def is_git_repo(repo_path):
+    return git.Repo(repo_path).git_dir is not None
+
+def get_remote_branches_info(repo_path):
+    repo = git.Repo(repo_path)
+    remote_branches = repo.remote().refs
+
+    branch_info = []
+    for branch in remote_branches:
+        if branch.remote_head == "HEAD":
+            continue
+
+        commits_diff = repo.git.rev_list('--left-right', '--count', f'origin/main...{branch.name}')
+        num_ahead, num_behind = commits_diff.split('\t')
+        branch_info.append( [branch.name, num_ahead, num_behind])
+        
+    for branch, behind, ahead in branch_info:
+        print(f"Branch: {branch}")
+        print(f"Commits behind main: {behind}")
+        print(f"Commits ahead of main: {ahead}")
+        print()
+
+    return branch_info
